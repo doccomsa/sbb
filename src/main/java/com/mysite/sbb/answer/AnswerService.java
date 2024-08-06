f5282ac0c01b62f5c8ecac74ee2029b8ca5b7cf0
doccomsa <newcomsa@nate.com>
2024-08-06 15:39:06 +0900

    질문및답변하기 완료

---
+++ src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -0,0 +1,28 @@
+package com.mysite.sbb.answer;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Service;
+
+import com.mysite.sbb.question.Question;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class AnswerService {
+
+	private final AnswerRepository answerRepository;
+	
+	//질문글에 대한 답변저장
+	public void create(Question question, String content) {
+		Answer answer = new Answer();
+		answer.setContent(content);
+		answer.setCreateDate(LocalDateTime.now());
+		
+		// 질문글에 대한 정보
+		answer.setQuestion(question);
+		
+		this.answerRepository.save(answer);
+	}
+}
--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,12 +4,21 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.mysite.sbb.answer.AnswerForm;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor // 생성자 메서드를 만들어주소, 생성자 주입방식이 진행된다.
 @Controller
 public class QuestionController {
@@ -17,7 +26,7 @@ public class QuestionController {
 	// 기본구조는 QuestionService 를 사용해야 하지만, 현재는 직접 리포지터를 사용한다.
 	private final QuestionService questionService;
 	
-	@GetMapping("/question/list")
+	@GetMapping("/list")
 //	@ResponseBody
 	public String list(Model model) {
 		
@@ -28,15 +37,36 @@ public class QuestionController {
 	}
 	
 	// 주소   /question/detail/1
-	@GetMapping(value = "/question/detail/{id}")
-	public String detail(Model model, @PathVariable("id") Integer id) {
+	@GetMapping(value = "/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
 		
 		Question question = this.questionService.getQuestion(id);
 		
 		model.addAttribute("question", question);
 		
-		return "question_detail";
+		return "question_detail"; // 답변 폼이 존재하기때문에 파라미터로 AnswerForm answerForm 사용한다.
 	}
+	
+	@GetMapping("/create")
+	public String questionCreate(QuestionForm questionForm) {
+		return "question_form";
+	}
+	
+	
+	@PostMapping("/create")
+	public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult ) {
+		
+		
+		if(bindingResult.hasErrors()) {
+			System.out.println("폼입력에러");
+			return "question_form";
+		}
+		
+		this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+		
+		return "redirect:/question/list";
+	}
+	
 	
 	
 	
