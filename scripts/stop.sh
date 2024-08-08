cc1949aadd0062adc66f493aa45e23f3de282d1d
doccomsa <newcomsa@nate.com>
2024-08-08 17:12:40 +0900

    deploy 11

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,8 +3,9 @@
 
 PROJECT="sbb-1.0.0"
 PROJECT_NAME=sbb
-PROJECT_FULL_PATH_01="/home/ec2-user/app/step1/sbb/build/libs"
-JAR_FILE="$PROJECT_FULL_PATH_01/$PROJECT.jar"
+PROJECT_FULL_PATH_01="/home/ec2-user/app/step1"
+JAR_LIB="/sbb/build/libs"
+JAR_FILE="$PROJECT_FULL_PATH_01$JAR_LIB/$PROJECT.jar"
 LOG_PATH="$PROJECT_FULL_PATH_01/logs"
 
 DEPLOY_LOG="$LOG_PATH/0_deploy.log"
