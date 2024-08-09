ef69a3ed862d598d4b2821c9e302aa1e207da2b4
doccomsa <newcomsa@nate.com>
2024-08-09 20:10:27 +0900

    deploy 13

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,8 +3,8 @@
 
 PROJECT="sbb-1.0.0"
 PROJECT_FULL_PATH_01="/home/ec2-user/app/step1/sbb"
-#JAR_LIB="/sbb/build/libs"
-JAR_FILE="$PROJECT_FULL_PATH_01/$PROJECT.jar"
+JAR_LIB="/sbb/build/libs"
+JAR_FILE="$PROJECT_FULL_PATH_01$JAR_LIB/$PROJECT.jar"
 LOG_PATH="$PROJECT_FULL_PATH_01/logs"
 
 APP_LOG="$LOG_PATH/application.log"
